b3888db4894ebafeb7e7779a8c6ec57a057453f7
pipu-kun <SideActivity@proton.me>
2026-04-16 10:33:02 +0700

    Add Element Input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,4 +5,14 @@ int element[10];
 int nPanjang;
 int x;
 
-void input(){}
+void input(){
+    while (true){
+        if (nPanjang <= 0){
+            break;
+        }else{
+            cout << "\nJumlah element tidak boleh lebih dari 10.\n";
+        }
+    }
+
+    
+}
